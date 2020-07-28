d3fd0254cca4b074726ccf013574b4da69752746
Keith Kurak <keith.kurak@gmail.com>
2020-07-28 13:39:01 -0400

    try without build step

---
+++ index.js
@@ -0,0 +1,5 @@
+import InboxScreen from './app/storyboard/InboxScreen';
+import ChatScreen from './app/storyboard/ChatScreen';
+import SettingsScreen from './app/storyboard/SettingsScreen';
+
+export { InboxScreen, ChatScreen, SettingsScreen };
