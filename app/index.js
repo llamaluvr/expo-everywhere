52d2a419a4e15a32760d765ab6a45d6f1edb105a
Keith Kurak <keith.kurak@gmail.com>
2020-07-27 23:46:24 -0400

    messed up exports

--- app/index.js
+++ app/index.js
@@ -2,4 +2,4 @@ import InboxScreen from './storyboard/InboxScreen';
 import ChatScreen from './storyboard/ChatScreen';
 import SettingsScreen from './storyboard/SettingsScreen';
 
-export default { InboxScreen, ChatScreen, SettingsScreen };
+export { InboxScreen, ChatScreen, SettingsScreen };
